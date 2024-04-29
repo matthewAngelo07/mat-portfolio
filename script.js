2364005c339dd56c7e095d9d41bdcfa9e0eb754c
matthewAngelo07 <158718974+matthewAngelo07@users.noreply.github.com>
2024-04-29 22:39:13 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Header toggle
-
-let MenuBtn = document.getElementById('MenuBtn')
-
-MenuBtn.addEventListener('click', function(e) {
-  document.querySelector('body').classList.toggle('mobile-nav-active');
-  this.classList.toggle('fa-xmark')
-})
